a1b44ed12b1f52221f05114326083ba58d729dc0
[Georgi Karchev] <georgikarcev@gmail.com>
2023-11-19 23:38:05 -0500

    Prime numbers solution

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ If a number is divisible by 5, log “Buzz.”
 If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 If a number is not divisible by either 3 or 5, log the number.
  */
-console.log(`Fizz Buzz`);
+console.log(`---------------Fizz Buzz------------`);
 for(let i=1; i<=100; i++) {
     if(i%3 ==0 && i%5==0){
         console.log("Fizz Buzz");
@@ -15,5 +15,26 @@ for(let i=1; i<=100; i++) {
         console.log("Buzz");
     } else {
         console.log(i);
+    }
+}
+console.log(`--------Prime numbers-------------`)
+//Write a script that accomplishes the following:
+//Declare an arbitrary number, n.
+//Create a loop that searches for the next prime number, starting at n and incrementing from there.
+//As soon as you find the prime number, log that number and exit the loop.
+ 
+
+for (let n =5; n<=27; n++){
+    let isPrime = true;
+
+    for  (let i=2; i<n; i++) {
+        //i=2,3,4
+        if(n%i ==0) {
+            isPrime = false; // mot a prime number
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(`${n} is a prime number.`);
     }
 }
